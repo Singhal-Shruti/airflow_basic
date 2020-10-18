3b3cdc46cdb00e7b178b2b162771a768dc2b7e2d
shrutisinghal <shruti.cs.du.2013@gmail.com>
2020-10-18 15:55:43 +0530

    basic airflow set up

---
+++ dags/helloworld.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.operators import BashOperator
+from datetime import datetime, timedelta
+
+# Following are defaults which can be overridden later on
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2018, 1, 1),
+    'email': ['airflow@thoughtworks.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
+}
+
+dag = DAG('HelloWorld', default_args=default_args)
+
+t1 = BashOperator(
+    task_id='task_1',
+    bash_command='echo "Hello World from Task 1"',
+    dag=dag)
+
+t2 = BashOperator(
+    task_id='task_2',
+    bash_command='echo "Hello World from Task 2"',
+    dag=dag)
+
+t3 = BashOperator(
+    task_id='task_3',
+    bash_command='echo "Hello World from Task 3"',
+    dag=dag)
+
+t4 = BashOperator(
+    task_id='task_4',
+    bash_command='echo "Hello World from Task 4"',
+    dag=dag)
+
+t1 >> t2 >> t4
+t1 >> t3 >> t4
